9e4630d512a1f182ca359f9217f47db0846563e0
Nimratcheema03 <nimratcheema03@gmail.com>
2023-04-04 09:02:24 -0700

    modify connect db to see why render cannot connect to mongodb

--- server code/connectDB.js
+++ server code/connectDB.js
@@ -5,14 +5,14 @@ dotenv.config();
 
 const connectDB = async (input) => {
   try {
-    const x = await mongoose.connect(process.env.DB_STRING)
+    await mongoose.connect(process.env.DB_STRING)
     console.log("Connected to db");
     if (input.drop === true)
       mongoose.connection.db.dropDatabase();
     // console.log("Dropped db");
     // get the data from Github 
   } catch (error) {
-    console.log('db error');
+    console.log(error);
   }
 }
 
